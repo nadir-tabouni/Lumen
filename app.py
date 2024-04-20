b48c5e614d577a13969beb8d014ff8fea4edacfc
nadir-tabouni <s_tabouni22@hwr-berlin.de>
2024-04-20 15:19:19 +0200

    implement basic login/register functionality

--- app.py
+++ app.py
@@ -1,18 +1,81 @@
-from flask import Flask
+from fastapi.templating import Jinja2Templates
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
+templates = Jinja2Templates(directory="webapp/templates")
+
+# db config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lumenDB.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+
+@app.before_request
+def create_tables():
+    db.create_all()
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 
-@app.route('/lists/')
-def lists():
-    return 'Todo: implement business logic to show all to-do lists'
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if username or email already exists
+        user_exists = User.query.filter((User.username == username) | (User.email == email)).first()
+        if user_exists:
+            return "Benutzername oder E-Mail bereits verwendet!"
+
+        # Hash the password
+        hashed_password = generate_password_hash(password)
+
+        # Insert new user into the database
+        new_user = User(username=username, email=email, password_hash=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/login')
+    return render_template('register.html')
 
 
-@app.route('/lists/<int:id>')
-def list(id):
-    return 'Todo: implement business logic to show all to-dos of a particular list'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the user exists and the password is correct
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password_hash, password):
+            session['user_id'] = user.id
+            return redirect('/')
+        else:
+            return 'Ungültige Anmeldedaten!'
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- migrations/sql/create_tables.sql
+++ migrations/sql/create_tables.sql
@@ -1,16 +1,7 @@
-CREATE TABLE todo (
-                      id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      complete BOOLEAN DEFAULT FALSE,
-                      description TEXT NOT NULL
-);
-CREATE TABLE list (
-                      id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      name TEXT NOT NULL
-);
-CREATE TABLE todo_list (
-                           todo_id INTEGER,
-                           list_id INTEGER,
-                           PRIMARY KEY (todo_id, list_id),
-                           FOREIGN KEY (todo_id) REFERENCES todo (id) ON DELETE CASCADE,
-                           FOREIGN KEY (list_id) REFERENCES list (id) ON DELETE CASCADE
+CREATE TABLE users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL UNIQUE,
+                        password TEXT NOT NULL,
+                        email TEXT UNIQUE,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
